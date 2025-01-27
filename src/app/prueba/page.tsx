7fd8709170caad78609b876730bffa5974d613f7
Nuria8890 <107542588+Nuria8890@users.noreply.github.com>
2025-01-27 13:02:13 +0100

    add paragraph (#6)

--- src/app/prueba/page.tsx
+++ src/app/prueba/page.tsx
@@ -4,6 +4,7 @@ import { Calendar } from "../components/calendar";
 export default function Prueba() {
   return (
     <>
+      <p>esto es una prueba de cierre de issue</p>
       <Archivo />
       <Calendar />
     </>
