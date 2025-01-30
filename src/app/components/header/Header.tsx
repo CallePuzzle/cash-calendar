c02a0d9d6d3eb7009914253f7811ca6cd35ba9f4
Nuria Campo <nuria.campo7@gmail.com>
2025-01-30 20:09:28 +0100

    burger menu styles

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,9 +1,10 @@
 "use client";
 import React, { useState } from "react";
+import "@/app/styles/components/Header.scss";
 import Image from "next/image";
 import logo from "@/app/images/logo_cash_calendar2.png";
 import burger from "@/app/images/burger_menu.png";
-import Nav from "./Nav";
+import Nav_ul from "./Nav_ul";
 
 function Header() {
   const [open, setOpen] = useState(false);
@@ -25,8 +26,8 @@ function Header() {
           <div className="nav__burger">
             <Image src={burger} alt="burger menu" onClick={handleOpenClose} />
           </div>
-          {open && <Nav isBurgerNav={true} />}
-          <Nav isBurgerNav={false} />
+          {open && <Nav_ul isBurgerNav={true} />}
+          <Nav_ul isBurgerNav={false} />
         </nav>
       </header>
     </>
---
+++ src/app/components/header/Nav_ul.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Link from "next/link";
+import "@/app/styles/components/Nav_ul.scss";
+
+function Nav_ul({ isBurgerNav }: { isBurgerNav: boolean }) {
+  return (
+    <ul className={isBurgerNav ? "nav__menu__burger" : "nav__menu"}>
+      <li>
+        <Link className="nav__menu__link" href="/welcome">
+          Bienvenida
+        </Link>
+      </li>
+      <li>
+        <Link className="nav__menu__link" href="/">
+          Mi calendario
+        </Link>
+      </li>
+      <li>
+        <Link className="nav__menu__link" href="">
+          Categorías
+        </Link>
+      </li>
+      <li>
+        <Link className="nav__menu__link" href="">
+          Estadísticas
+        </Link>
+      </li>
+      <li>
+        <Link className="nav__menu__link" href="">
+          Mi perfil
+        </Link>
+      </li>
+    </ul>
+  );
+}
+
+export default Nav_ul;
