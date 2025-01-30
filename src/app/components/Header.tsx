aa4a141ba3d1e2a13f6ec76f0bdc08de3456163e
Nuria Campo <nuria.campo7@gmail.com>
2025-01-30 13:31:16 +0100

    add burguer menu and media queries

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,15 +3,19 @@ import "../styles/components/Header.scss";
 import Link from "next/link";
 import Image from "next/image";
 import logo from "@/app/images/logo_cash_calendar2.png";
+import burguer from "@/app/images/burguer_menu.png";
 
 function Header() {
   return (
     <>
       <header className="header">
         <div className="header__logo">
-          <Image src={logo} alt="logo de cash calendar" />
+          <Image src={logo} alt="cash calendar logo" />
         </div>
         <nav className="nav">
+          <div className="nav__burguer">
+            <Image src={burguer} alt="burguer menu" />
+          </div>
           <ul className="nav__menu">
             <li>
               <Link className="nav__menu__link" href="/welcome">
