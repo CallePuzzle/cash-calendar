39ff3498648862bda38b4b9cde89aa2f57a8fb94
Nuria Campo <nuria.campo7@gmail.com>
2025-01-31 11:39:11 +0100

    add arrow up

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "../styles/components/Footer.scss";
 import Link from "next/link";
+import Image from "next/image";
+import arrow_up from "@/app/images/arrow_up.png";
 
 function Footer() {
   return (
     <footer className="footer">
       <Link href="#header">
-        <div className="footer__arrow">⬆️</div>
+        <div className="footer__arrow">
+          <Image src={arrow_up} alt="arrow up" className="w-6" />
+        </div>
       </Link>
 
       <div className="footer__copy">
