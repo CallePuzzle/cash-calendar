570a24fdd2d90f58de7b7a9340649e839c333b2d
Nuria Campo <nuria.campo7@gmail.com>
2025-01-31 11:52:26 +0100

    add logo and place content in rows

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,6 +3,7 @@ import "../styles/components/Footer.scss";
 import Link from "next/link";
 import Image from "next/image";
 import arrow_up from "@/app/images/arrow_up.png";
+import logo from "@/app/images/logo_cash_calendar2.png";
 
 function Footer() {
   return (
@@ -12,10 +13,14 @@ function Footer() {
           <Image src={arrow_up} alt="arrow up" className="w-6" />
         </div>
       </Link>
-
-      <div className="footer__copy">
-        <p>&copy; Nuriant 🐜 2025</p>
-        <p>I 🖤 code</p>
+      <div className="footer__content">
+        <div className="footer__content__copy">
+          <p>&copy; Nuriant 🐜 2025</p>
+          <p>I 🖤 code</p>
+        </div>
+        <div className="footer__content__logo">
+          <Image src={logo} alt="cash calendar logo" />
+        </div>
       </div>
     </footer>
   );
