82180329edb1c2c83d3e156847a14ef028944583
Nuria Campo <nuria.campo7@gmail.com>
2025-02-09 20:00:29 +0100

    reset calendar styles

--- src/app/components/calendar.tsx
+++ src/app/components/calendar.tsx
@@ -3,13 +3,20 @@ import { useState } from "react";
 
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/style.css";
+import "@/app/styles/components/calendar.scss";
 
 export function Calendar() {
   const [selected, setSelected] = useState<Date>();
-
+  // const defaultClassNames = getDefaultClassNames();
   return (
     <DayPicker
       mode="single"
+      // classNames={{
+      //   today: `border-red-500`, // Add a border to today's date
+      //   selected: `bg-red-500 border-amber-500 text-white`, // Highlight the selected day
+      //   root: `${defaultClassNames.root} shadow-lg p-5`, // Add a shadow to the root element
+      //   chevron: `${defaultClassNames.chevron} fill-amber-500`, // Change the color of the chevron
+      // }}
       selected={selected}
       onSelect={setSelected}
       footer={
