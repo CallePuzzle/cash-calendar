41d0b87a8a468f31fdc38a0646f554d77bf43929
Nuria8890 <107542588+Nuria8890@users.noreply.github.com>
2025-02-10 17:32:44 +0100

    5 base calendar markup (#12)
    * change styles calendar
    
    * change styles calendar
    
    * new calendar version
    
    * reset calendar styles
    
    * change today color
    
    * media quieries
    
    * calendar mobile styles ok
    
    * calendar tablet styles ok
    
    * change width of the logo
    
    * calendar desktop styles ok

--- src/app/components/calendar.tsx
+++ src/app/components/calendar.tsx
@@ -1,18 +1,27 @@
 "use client";
+import { useState } from "react";
 
-import * as React from "react";
-
-import { Calendar as CalendarUI } from "@/components/ui/calendar";
+import { DayPicker, getDefaultClassNames } from "react-day-picker";
+import "react-day-picker/style.css";
+import "@/app/styles/components/calendar.scss";
 
 export function Calendar() {
-  const [date, setDate] = React.useState<Date | undefined>(new Date());
-
+  const [selected, setSelected] = useState<Date>();
+  const defaultClassNames = getDefaultClassNames();
   return (
-    <CalendarUI
+    <DayPicker
       mode="single"
-      selected={date}
-      onSelect={setDate}
-      className="rounded-md border shadow"
+      classNames={{
+        day: `${defaultClassNames.day} md:text-5xl`,
+        weekdays: `${defaultClassNames.weekdays} md:text-5xl`,
+        caption_label: `${defaultClassNames.caption_label} md:text-4xl`,
+        footer: `${defaultClassNames.footer} md:text-3xl lg:text-xl`,
+      }}
+      selected={selected}
+      onSelect={setSelected}
+      footer={
+        selected ? `Selected: ${selected.toLocaleDateString()}` : "Pick a day."
+      }
     />
   );
 }
