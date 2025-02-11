39de8dcc51ddfac88c146c95d7408bc57387c7b2
Nuria Campo <nuria.campo7@gmail.com>
2025-02-11 11:34:04 +0100

    create and import DayDetail component

---
+++ src/app/components/DayDetail.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function DayDetail() {
+  return <div>soy el detalle del día</div>;
+}
+
+export default DayDetail;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Calendar } from "@/app/components/Calendar";
+import DayDetail from "./components/DayDetail";
 
 export default function Index() {
   return (
-    <>
+    <section className="flex flex-col items-center justify-around">
       <Calendar />
-    </>
+      <DayDetail />
+    </section>
   );
 }
