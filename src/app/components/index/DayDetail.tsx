0a384999d434aefdaa8636cb124f514a4f18e6a4
Nuria8890 <107542588+Nuria8890@users.noreply.github.com>
2025-02-19 10:46:28 +0100

    13 day details (#16)
    * fix unused component
    
    * change calendar to Calendar
    
    * change calendar to Calendar
    
    * create and import DayDetail component
    
    * media quieries 1040 in Index component
    
    * delete console.log
    
    * create logic of painting cost per day
    
    * add margin button in DayDetail component
    
    * move the selectedDay state variable to the page component and type the data
    
    * created function to stringifyed day selected
    
    * take the day of the calendar and check if there are expenses generated
    
    * delete Calendar from Demo, and add absolute routes

---
+++ src/app/components/index/DayDetail.tsx
@@ -0,0 +1,79 @@
+"use client";
+import React from "react";
+import Detail from "@/app/components/index/Detail";
+
+function getDayDetails(day: string) {
+  const dailyExpenses = {
+    "11/2/2025": [
+      {
+        id: 1,
+        name: "cañas",
+        quantity: 15,
+        categoria: "bares y restaurantes",
+      },
+      {
+        id: 2,
+        name: "camiseta",
+        quantity: 20,
+        categoria: "ropa",
+      },
+    ],
+    "4/2/2025": [
+      {
+        id: 3,
+        name: "comida",
+        quantity: 50,
+        categoria: "bares y restaurantes",
+      },
+      {
+        id: 4,
+        name: "pantalon",
+        quantity: 35,
+        categoria: "ropa",
+      },
+    ],
+  };
+
+  if (day in dailyExpenses) {
+    return dailyExpenses[day as keyof typeof dailyExpenses];
+  }
+  return [];
+}
+function getSelectedDayToString(selectedDate: Date | undefined) {
+  if (selectedDate) {
+    const dayStringyfied = selectedDate.getDate();
+
+    const monthStringyfied = selectedDate.getMonth() + 1;
+
+    const yearStringyfied = selectedDate.getFullYear();
+
+    return `${dayStringyfied}/${monthStringyfied}/${yearStringyfied}`;
+  } else {
+    return "";
+  }
+}
+
+function DayDetail({ selectedDay }: { selectedDay: Date | undefined }) {
+  const selectedDayToString = getSelectedDayToString(selectedDay);
+
+  const expenses = getDayDetails(selectedDayToString);
+
+  const expensesElement = expenses.map((expense) => {
+    return <Detail key={expense.id} expense={expense} />;
+  });
+  return (
+    <ul className="mb-6 ml:mb-0">
+      {selectedDay
+        ? `Día seleccionado: ${selectedDayToString}`
+        : "Selecciona un día"}
+      {expensesElement}
+      <p>
+        {selectedDayToString && expensesElement.length === 0
+          ? "Hoy no he generado ningún gasto"
+          : ""}
+      </p>
+    </ul>
+  );
+}
+
+export default DayDetail;
