7e94c4fd0813f49efe4c6f016a53449a44cd3e52
Nuria Campo <nuria.campo7@gmail.com>
2025-02-20 19:34:06 +0100

    bgColor variable declarated

--- src/app/components/index/Detail.tsx
+++ src/app/components/index/Detail.tsx
@@ -8,8 +8,9 @@ interface Expense {
 }
 
 function Detail({ expense }: { expense: Expense }) {
+  const bgColor = `bg-[${expense.color}]`;
   return (
-    <li className={`bg-[${expense.color}] m-px p-px rounded-lg w-[60%] `}>
+    <li className={`${bgColor} m-px p-px rounded-lg w-[60%] `}>
       <div className="p-3 flex flex-row gap-10 justify-between">
         <p>
           {expense.quantity}€ en {expense.name}
