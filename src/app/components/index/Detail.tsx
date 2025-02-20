5dea084293cd8f9191fd6b2afee676b7aa478819
Nuria Campo <nuria.campo7@gmail.com>
2025-02-20 19:55:55 +0100

    created bgColorClass category's field

--- src/app/components/index/Detail.tsx
+++ src/app/components/index/Detail.tsx
@@ -3,14 +3,13 @@ import React from "react";
 interface Expense {
   name: string;
   quantity: number;
-  color: string;
+  bgColorClass: string;
   category: string;
 }
 
 function Detail({ expense }: { expense: Expense }) {
-  const bgColor = `bg-[${expense.color}]`;
   return (
-    <li className={`${bgColor} m-px p-px rounded-lg w-[60%] `}>
+    <li className={`${expense.bgColorClass} m-px p-px rounded-lg w-[60%] `}>
       <div className="p-3 flex flex-row gap-10 justify-between">
         <p>
           {expense.quantity}€ en {expense.name}
