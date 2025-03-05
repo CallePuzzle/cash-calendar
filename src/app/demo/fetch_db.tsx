2e24067a8fae9b7307f191202c32f97f56e2bbd2
Nuria Campo <nuria.campo7@gmail.com>
2025-03-05 13:06:00 +0100

    create fetch_db component

---
+++ src/app/demo/fetch_db.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function fetch_db() {
+  return (
+    <>
+      <p>Aquí voy a pintar los usuarios que me devuelva la db</p>
+      <ul>
+        <li>Id:</li>
+        <li>Nombre:</li>
+        <li>Email:</li>
+      </ul>
+    </>
+  );
+}
+
+export default fetch_db;
