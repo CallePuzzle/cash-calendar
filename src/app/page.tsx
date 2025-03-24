0acc3e0728fb42222ee321f63038e0dda27ddb14
Nuria Campo <nuria.campo7@gmail.com>
2025-03-24 20:14:01 +0100

    when scroll the menu down

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import DayDetail from "@/app/components/index/DayDetail";
 export default function Index() {
   const [selectedDay, setSelectedDay] = useState<Date>();
   return (
-    <section className="min-w-320 flex flex-col items-center justify-around lg:flex-row lg:grid lg:grid-cols-2 lg:grid-rows-1">
+    <section className="mt-[5.4rem] min-w-320 flex flex-col items-center justify-around lg:flex-row lg:grid lg:grid-cols-2 lg:grid-rows-1">
       <Calendar selected={selectedDay} setSelected={setSelectedDay} />
       <DayDetail selectedDay={selectedDay} />
     </section>
